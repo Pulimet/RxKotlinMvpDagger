fc65d047fc9959077cc58b5761cd435d57fa0bcc
Alexey Korolev <korolev2009@gmail.com>
2018-04-22 09:31:06 +0300

    Add CompositeDisposable

--- app/src/main/java/net/alexandroid/utils/rxkotlinmvpdagger/MainActivity.kt
+++ app/src/main/java/net/alexandroid/utils/rxkotlinmvpdagger/MainActivity.kt
@@ -8,26 +8,45 @@ import android.text.TextWatcher
 import io.reactivex.Observable
 import io.reactivex.ObservableOnSubscribe
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.concurrent.TimeUnit
 
+
 class MainActivity : AppCompatActivity() {
 
-    @SuppressLint("CheckResult")
+    private val disposables = CompositeDisposable()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Observable.create(ObservableOnSubscribe<String> { subscriber ->
-            editText.addTextChangedListener(object : TextWatcher {
-                override fun afterTextChanged(s: Editable?) = Unit
-                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) = Unit
-                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) = subscriber.onNext(s.toString())
-            })
-        }).debounce(1000, TimeUnit.MILLISECONDS)
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe({ text ->
-                    textView.text = text
-                })
+        setSearchListener()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        disposables.clear()
+    }
+
+    @SuppressLint("CheckResult")
+    private fun setSearchListener() {
+        disposables.add(
+                Observable.create(ObservableOnSubscribe<String> { subscriber ->
+                    editText.addTextChangedListener(object : TextWatcher {
+                        override fun afterTextChanged(s: Editable?) = Unit
+                        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) = Unit
+                        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) = subscriber.onNext(s.toString())
+                    })
+                }).debounce(1000, TimeUnit.MILLISECONDS)
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe({ text ->
+                            textView.text = text
+                            onNewSearchQuery()
+                        }))
+    }
+
+    private fun onNewSearchQuery() {
+
     }
 }
